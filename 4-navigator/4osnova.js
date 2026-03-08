6c9a51f71f481dd7109be14a8d87e8acd838c0b2
Islam <islam1234567891011121314@gmail.com>
2026-03-08 15:59:52 +0400

    домашнее задание 4 главы

---
+++ 4-navigator/4osnova.js
@@ -0,0 +1,12 @@
+const x1 = 11;
+const x2 = 124;
+const y1 = -151;
+const y2 = 201;
+console.log((((x2 - x1)**2) + ((y2 - y1)**2)) ** 0.5)
+
+function distance(addressLat, addressLong,positionLat,positionLong){
+    const x2x1 = (addressLong - positionLong)**2;
+    const y2y1 = (addressLat - positionLat)**2;
+    return (x2x1 + y2y1) ** 0.5
+}
+console.log(distance(11, 201, 124, -151))
