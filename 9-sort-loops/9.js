4f8adafb2cf1802a68c1a4dfa4e7879978827af5
Islam <islam1234567891011121314@gmail.com>
2026-03-13 16:24:52 +0400

    9 дз

---
+++ 9-sort-loops/9.js
@@ -0,0 +1,17 @@
+
+function bubbleSort(arr) {
+  const n = arr.length;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+  return arr;
+}
+
+const arr = [1, 40, -5, 10, 0];
+console.log(bubbleSort(arr));
